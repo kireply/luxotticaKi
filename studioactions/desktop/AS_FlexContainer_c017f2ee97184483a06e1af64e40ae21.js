f27a35ecec81e524dc658e745c22812acb131350
JonathanCerulloUyi <j.cerullouyi@reply.it>
2024-03-04 17:41:28 +0100

    Funzionalità Duplicate flow
    Aggiunta funzionalità di duplicazione del flusso selezionato. Da aggiungere funzionalità delle altre icone e visualizzazione corretta (hover su icona, popup del nome icona in alto da aggiungere)

---
+++ studioactions/desktop/AS_FlexContainer_c017f2ee97184483a06e1af64e40ae21.js
@@ -0,0 +1,5 @@
+function AS_FlexContainer_c017f2ee97184483a06e1af64e40ae21(eventobject) {
+    var self = this;
+    voltmx.print("### cliccato LAYOUT");
+    this.view.flxStepsLayout.setFocus(true);
+}
