54cbc3bfadb67645aa371ff9e3225866f049bab7
JonathanCerulloUyi <j.cerullouyi@reply.it>
2024-03-05 17:48:16 +0100

    Merge con aggiornamenti Eugi
    Merge con aggiornamenti/modifiche di Eugi per visualizzazione orizzontale dei Channels nel Form frmChannel (controller, component con contratto ecc...)

--- controllers/desktop/frmChannelsController/frmChannelsController.js
+++ controllers/desktop/frmChannelsController/frmChannelsController.js
@@ -1,5 +1,129 @@
-define({ 
-
- //Type your controller code here 
-
- });
+define({
+ 
+//   tiles:[],
+  channels:[],
+ 
+  onViewCreated(){
+//     this.view.init = () => {
+ 
+//       this.tiles = data.tiles;
+// //       this.view.btnAdd.onClick = () =>{
+// //         this.addTile(this.tiles[0],0);
+// //       }
+//     };
+    this.view.preShow = async () => {
+      //try{
+        await this.fetchChannels(); // aspetta che fetchChannels sia completato
+        voltmx.print("### CHANNELS INSIDE ON VIEW: " + JSON.stringify(this.channels));
+        this.reloadData();
+    };
+  },
+ 
+  reloadData(){
+//     voltmx.print("### CHANNELS INSIDE ON RELOAD: " + JSON.stringify(this.channels));
+    this.view.flxChannels.removeAll();
+    this.channels.forEach((box, index) => {this.addTile(box,index)});
+    this.addTile(null, 99);
+  },
+  fetchChannels(){
+    return new Promise((resolve, reject) => {
+      voltmx.print("### INSIDE fetchChannels");
+      var integrationService = null;
+      var sdkDefaultInstance = voltmx.sdk.getDefaultInstance();
+      var integrationService = sdkDefaultInstance.getIntegrationService("mariaDB");
+ 
+      integrationService.invokeOperation("CHANNEL_get",{},{},
+         (response) => {
+          voltmx.application.showLoadingScreen(null, "Searching channels ...", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
+          voltmx.print ("### Service response: "+JSON.stringify(response));
+          voltmx.print ("### CHANNELS: " + JSON.stringify(response.CHANNEL));
+          /*
+          CHANNELS: [{"name":"DolceGabbana","logo":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ray-Ban_logo.svg/772px-Ray-Ban_logo.svg.png","id":"DG","properties_file":"p4"},{"name":"RayBan","logo":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ray-Ban_logo.svg/772px-Ray-Ban_logo.svg.png","id":"RB","properties_file":"p5"}]
+          */
+          this.channels = response.CHANNEL;
+          voltmx.print("### CHANNELS ATTRIBUTE: " + JSON.stringify(this.channels));
+          voltmx.application.dismissLoadingScreen();
+          resolve(this.channels);
+         },
+         (error) => {
+          voltmx.print("### Error in the invocation of the service: " + JSON.stringify(error));
+          reject(error);
+        }
+      );
+    });
+  },
+  addTile(box, index){
+      voltmx.print("### INSIDE addTile");
+      voltmx.print("### BOX: " + JSON.stringify(box));
+      /*
+      BOX: {"name":"DolceGabbana","logo":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ray-Ban_logo.svg/772px-Ray-Ban_logo.svg.png","id":"DG","properties_file":"p4"}
+      */
+      voltmx.print("### INDEX: " + JSON.stringify(index));
+      const flex = new voltmx.ui.FlexContainer({
+        id: `flex${index}${new Date().getTime()}`,
+        height: `130dp`,
+        responsiveConfig: {
+          "span": {
+            "640": 12,
+            "1024": 6,
+            "1366": 4
+          }
+        },
+      }, {}, {});
+//       voltmx.print("### BUILDED FLEX");
+      //const tile = new com.hcl.demo.uifactory.Tile({
+      const channel = new ki.luxottica.channelTemplatewithContract({
+        id: `channel{index}${new Date().getTime()}`,
+        width: '95%',
+        height: '175dp',
+        centerX: '50%',
+        centerY: '50%'
+      }, {}, {});
+//       voltmx.print("### CREATO IL CHANNEL");
+      if (box !== null) {
+        channel.channelLogo = box.logo;
+      	channel.channelInfo = box.name;
+        channel.channelIdentifier = box.id;
+        channel.channelProperties = box.properties_file;
+        channel.channelId = index;
+        channel.onClickTeaser = () => {
+      	//TODO ACTIONS WHILE CLICKING ON THE TILE
+          if (gblInfoIcon === true){
+  //           voltmx.print("### SET VISIBILITY");         
+            this.view.flxPopupChannelInfo.txtChannelID.text = channel.channelIdentifier;
+            this.view.flxPopupChannelInfo.txtChannelLogo.text = channel.channelLogo;
+            this.view.flxPopupChannelInfo.txtChannelProperties.text = channel.channelProperties;
+            this.view.flxPopupChannelInfo.lbChannelName.masterData = [ ["lb1", channel.channelInfo] ];
+            this.view.flxPopupChannelInfo.lbChannelName.selectedKey = "lb1"
+            this.view.flxPopupChannelInfo.setVisibility(true);
+          } else {
+  //           voltmx.print("### NAVIGATE");
+            gblChannelLogo = box.logo;
+            gblChannelName = box.name;
+            gblChannelId = box.id;
+            voltmx.print("### GLOBAL INFO: " + gblChannelName);
+            voltmx.print("### GLOBAL LOGO: " + gblChannelLogo);
+            var navigationManager = new voltmx.mvc.Navigation("frmFlows");
+            navigationManager.navigate();
+          }
+        }
+      }
+      else {
+//        voltmx.print("### INSIDE ELSE");
+       channel.channelLogo = "/add_circle_icon.png";
+       channel.channelInfo = "Add New Channel";
+       channel.channelId = index;
+//        voltmx.print("### CHANNEL INFO: " + channel.channelInfo);
+//        voltmx.print("### CHANNEL LOGO: " + channel.channelLogo);
+//        voltmx.print("### CHANNEL ID: " + channel.channelId);
+//        channel.channelImgInfo.setVisibility(false);
+      }
+//       voltmx.print("### CHANNEL INFO: " + channel.channelInfo);
+//       voltmx.print("### CHANNEL LOGO: " + channel.channelLogo);
+//       voltmx.print("### CHANNEL ID: " + channel.channelId);
+      flex.add(channel);
+      this.view.flxChannels.add(flex);
+    this.view.flxChannels.forceLayout();
+  }
+ 
+});
